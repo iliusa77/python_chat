454e1fdd2c6bdc51bd424e4d86e897a64ad21a85
Ilia Eriomenco <ilia@alarstudios.com>
2019-01-17 12:30:17 +0100

    typofix

--- client.py
+++ client.py
@@ -44,7 +44,7 @@ rT.start()
 
 while shutdown == False:
 	if join == False:
-		s.sendto(("["+ alias +"] <= left chat ").encode("utf-8"), server)
+		s.sendto(("[" + alias + "] => join chat ").encode("utf-8"), server)
 		join = True
 	else:
 		try:
